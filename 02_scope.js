308a918c50704211cc60d7ff3e2a0421616118fa
amitesh3168 <amiteshsmb@gmail.com>
2025-02-12 23:58:26 +0530

    new

---
+++ 02_scope.js
@@ -0,0 +1,15 @@
+let a = 10
+const b = 2
+var c = 20
+console.log(a) 
+console.log(b)
+console.log(c)
+
+if (true){
+    let a = 10;
+    const b = 20
+    var c = 43
+}
+// console.log(a) 
+// console.log(b)it give error but when we use var in scope the you can access it from outside
+console.log(c)
